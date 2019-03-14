df7b140cef05bb911b76e6699152e48e1a6d2ec1
justin <Justinvd0203@gmail.com>
2019-03-14 10:11:51 +0100

    dataprofiel

--- dataprofiel.py
+++ dataprofiel.py
@@ -2,11 +2,11 @@ import pandas as pd
 import pandas_profiling
 
 def main():
-    file_location = 'Leidse_regio_data.csv'
-    df = pd.read_csv(file_location)
+    file_location = "D:\Downloads\Subsidie_opgeschoonde_data.csv"
+    df = pd.read_csv(file_location, sep=';')
 
     profile = pandas_profiling.ProfileReport(df)
-    profile.to_file(outputfile='output.html')
+    profile.to_file(outputfile='output_subsidies.html')
 
 if __name__ == '__main__':
     main()
